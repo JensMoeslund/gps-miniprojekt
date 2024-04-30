55d6683571d26f50e769614ad5691f99b2e8eed7
Albert Laursen <albert@bredenberg.dk>
2024-04-30 09:55:10 +0200

    børgos

--- src/lib.rs
+++ src/lib.rs
@@ -9,258 +9,38 @@ use nmea0183 as nmea;
 use panic_probe as _; // panic handler
 use stm32f4xx_hal as _; // memory layout // time abstractions
 pub mod ms5611;
-use heapless::String;
 #[derive(Debug)]
 pub enum Error {
-    InvalidTransition {
-        from: NmeaRecieverState,
-        to: NmeaRecieverState,
-    },
-
-    BufferFull {
-        byte: char,
-    },
-
-    SentenceNotStarted {
-        byte: char,
-    },
+    InvalidSentence,
 
     SentenceWithoutPositonData,
-
-    InvalidSentence,
-    
-    // Used as a return type for the handle_byte method to support while let loops
-    SentenceNotFinished,
 }
 
 impl Format for Error {
     fn format(&self, fmt: defmt::Formatter) {
         match self {
-            Error::InvalidTransition { from, to } => {
-                defmt::write!(
-                    fmt,
-                    "Invalid reciever state transition from {:?} to {:?}",
-                    from,
-                    to
-                )
-            }
-            Error::BufferFull { byte } => {
-                defmt::write!(fmt, "Buffer full, cannot push byte {}", byte)
-            }
-            Error::SentenceNotStarted { byte } => {
-                defmt::write!(fmt, "Sentence not started with {}", byte)
-            }
-            Error::SentenceNotFinished => {
-                defmt::write!(fmt, "NMEA sentence not yet finished (continue reading)")
-            }
             Error::SentenceWithoutPositonData => {
                 defmt::write!(fmt, "NMEA sentence does not contain position data")
-            },
+            }
             Error::InvalidSentence => {
                 defmt::write!(fmt, "Invalid NMEA sentence")
-            },
-        }
-    }
-}
-
-// Reciever finite state machine
-// The reciever can only go forward in state, and can only go back to the Clear state.
-// an invalid transition will result in returning an Error::InvalidTransition.
-#[derive(Format, Debug, Clone, Copy)]
-pub enum NmeaRecieverState {
-    Clear,         // Nothing recieved yet
-    StartRecieved, // Start of sentence recieved
-    Recieving,     // Recieving a sentence
-    CrRecieved,    // Carriage return recieved
-    LfRecieved,    // Line feed recieved (end of sentence)
-}
-
-//
-impl NmeaRecieverState {
-    pub fn new() -> Self {
-        NmeaRecieverState::Clear
-    }
-
-    fn clear(&mut self) {
-        *self = NmeaRecieverState::Clear;
-    }
-    fn start_recieved(&mut self) -> Result<(), Error> {
-        match self {
-            NmeaRecieverState::Clear => {
-                *self = NmeaRecieverState::StartRecieved;
-                Ok(())
-            }
-            _ => Err(Error::InvalidTransition {
-                from: *self,
-                to: NmeaRecieverState::StartRecieved,
-            }),
-        }
-    }
-
-    fn recieving(&mut self) -> Result<(), Error> {
-        match self {
-            NmeaRecieverState::StartRecieved => {
-                *self = NmeaRecieverState::Recieving;
-                Ok(())
-            }
-            _ => Err(Error::InvalidTransition {
-                from: *self,
-                to: NmeaRecieverState::Recieving,
-            }),
-        }
-    }
-
-    fn cr_recieved(&mut self) -> Result<(), Error> {
-        match self {
-            NmeaRecieverState::Recieving => {
-                *self = NmeaRecieverState::CrRecieved;
-                Ok(())
-            }
-            _ => Err(Error::InvalidTransition {
-                from: *self,
-                to: NmeaRecieverState::CrRecieved,
-            }),
-        }
-    }
-
-    fn lf_recieved(&mut self) -> Result<(), Error> {
-        match self {
-            NmeaRecieverState::CrRecieved => {
-                *self = NmeaRecieverState::LfRecieved;
-                Ok(())
-            }
-            _ => Err(Error::InvalidTransition {
-                from: *self,
-                to: NmeaRecieverState::LfRecieved,
-            }),
-        }
-    }
-}
-
-const BUF_LEN: usize = 82; // 82 is the maximum length of a NMEA sentence
-
-pub struct NmeaReciever {
-    state: NmeaRecieverState,
-    buffer: String<BUF_LEN>,
-}
-
-impl NmeaReciever {
-    pub fn new() -> Self {
-        NmeaReciever {
-            state: NmeaRecieverState::Clear,
-            buffer: String::new(),
-        }
-    }
-
-    fn clear(&mut self) {
-        self.state.clear();
-        self.buffer.clear();
-    }
-    fn start_recieved(&mut self) -> Result<(), Error> {
-        self.state.start_recieved()
-    }
-
-    fn recieving(&mut self) -> Result<(), Error> {
-        self.state.recieving()
-    }
-
-    fn cr_recieved(&mut self) -> Result<(), Error> {
-        self.state.cr_recieved()
-    }
-
-    fn lf_recieved(&mut self) -> Result<(), Error> {
-        self.state.lf_recieved()
-    }
-
-    fn push(&mut self, byte: char) -> Result<(), Error> {
-        self.buffer
-            .push(byte)
-            .map_err(|_| Error::BufferFull { byte })
-    }
-
-    pub fn handle_byte(&mut self, byte: char) -> Result<String<BUF_LEN>, Error> {
-        match self.state {
-            NmeaRecieverState::Clear => match byte {
-                '$' => {
-                    self.start_recieved()?;
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-                _ => {
-                    self.clear();
-                    Err(Error::SentenceNotStarted { byte })
-                }
-            },
-            NmeaRecieverState::StartRecieved => match byte {
-                '$' => {
-                    self.clear();
-                    self.start_recieved()?;
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-                _ => {
-                    self.recieving()?;
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-            },
-            NmeaRecieverState::Recieving => match byte {
-                '\r' => {
-                    self.cr_recieved()?;
-                    // self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-                '$' => {
-                    self.clear();
-                    self.start_recieved()?;
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-                _ => {
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-            },
-            NmeaRecieverState::CrRecieved => match byte {
-                '\n' => {
-                    self.lf_recieved()?;
-                    // self.push(byte)?;
-                    let sentence = self.buffer.clone();
-                    self.clear();
-                    Ok(sentence)
-                }
-                '$' => {
-                    self.clear();
-                    self.start_recieved()?;
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-                _ => {
-                    self.push(byte)?;
-                    Err(Error::SentenceNotFinished)
-                }
-            },
-            NmeaRecieverState::LfRecieved => {
-                // This should never happen, but we have to handle it
-                self.clear();
-                Err(Error::InvalidTransition {
-                    from: NmeaRecieverState::LfRecieved,
-                    to: NmeaRecieverState::Clear,
-                })
             }
         }
     }
 }
-#[derive(Format,Default,Clone, Copy)]
+
+#[derive(Format, Default, Clone, Copy)]
 pub struct GnssLocation {
     pub latitude: f64,
     pub longitude: f64,
     pub altitude: Option<f32>,
+    pub speed: Option<f32>,
+    pub course: Option<f32>,
 }
 
 impl TryFrom<nmea::ParseResult> for GnssLocation {
     type Error = Error;
-    fn try_from(value: nmea::ParseResult) -> Result<Self,Self::Error> {
+    fn try_from(value: nmea::ParseResult) -> Result<Self, Self::Error> {
         match value {
             nmea::ParseResult::RMC(Some(rmc)) => Ok(Self::from(rmc)),
             nmea::ParseResult::GGA(Some(gga)) => Ok(Self::from(gga)),
@@ -270,18 +50,31 @@ impl TryFrom<nmea::ParseResult> for GnssLocation {
             nmea::ParseResult::GLL(None) => Err(Error::InvalidSentence),
             _ => Err(Error::SentenceWithoutPositonData),
         }
-
     }
 }
+
+impl GnssLocation {
+    pub fn update(&mut self, value: nmea::ParseResult) -> Result<(), Error> {
+        let location = GnssLocation::try_from(value)?;
+        self.latitude = location.latitude;
+        self.longitude = location.longitude;
+        self.altitude = location.altitude.or(self.altitude);
+        self.speed = location.speed.or(self.speed);
+        self.course = location.course.or(self.course);
+        Ok(())
+    }
+}
+
 impl From<nmea::RMC> for GnssLocation {
     fn from(rmc: nmea::RMC) -> Self {
         GnssLocation {
             latitude: rmc.latitude.as_f64(),
             longitude: rmc.longitude.as_f64(),
             altitude: None,
+            speed: Some(rmc.speed.as_mps()),
+            course: rmc.course.map(|f| f.degrees),
         }
     }
-
 }
 impl From<nmea::GGA> for GnssLocation {
     fn from(gga: nmea::GGA) -> Self {
@@ -289,9 +82,10 @@ impl From<nmea::GGA> for GnssLocation {
             latitude: gga.latitude.as_f64(),
             longitude: gga.longitude.as_f64(),
             altitude: Some(gga.altitude.meters),
+            speed: None,
+            course: None,
         }
     }
-    
 }
 impl From<nmea::GLL> for GnssLocation {
     fn from(gll: nmea::GLL) -> Self {
@@ -299,12 +93,11 @@ impl From<nmea::GLL> for GnssLocation {
             latitude: gll.latitude.as_f64(),
             longitude: gll.longitude.as_f64(),
             altitude: None,
+            speed: None,
+            course: None,
         }
     }
-    
 }
-
-
 
 #[macro_export]
 macro_rules! configure_clock {
--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,31 @@
 #![no_main]
 #![no_std]
-// #![deny(warnings)]
+#![deny(warnings)]
 // #![feature(type_alias_impl_trait)]
 
 use gps_miniprojekt as _; // global logger + panicking-behavior
 use gps_miniprojekt::ms5611::Ms5611Sample;
-use gps_miniprojekt::{GnssLocation, NmeaReciever};
-use nmea0183::datetime::DateTime;
-
+use gps_miniprojekt::GnssLocation;
 
 struct SensorData {
     ms5611_data: Ms5611Sample,
     gnss_location: GnssLocation,
     // timestamp: DateTime,
 }
-impl SensorData {
-    pub fn new() -> Self {
-        todo!();
-    }
 
-}
-
-
-const BUF_LEN: usize = 20; 
+const BUF_LEN: usize = 20;
 impl defmt::Format for SensorData {
     fn format(&self, f: defmt::Formatter) {
         defmt::write!(
             f,
-            "{},{},{},{},{}",
+            "{},{},{},{},{},{},{}",
             self.ms5611_data.temperature_c,
             self.ms5611_data.pressure_mbar,
             self.gnss_location.latitude,
             self.gnss_location.longitude,
-            self.gnss_location.altitude 
+            self.gnss_location.altitude,
+            self.gnss_location.speed,
+            self.gnss_location.course,
         );
     }
 }
@@ -45,15 +38,13 @@ mod app {
     use super::*;
     // use alloc::string::ToString;
     use defmt::info;
-    use gps_miniprojekt::{
-        ms5611::{self, Osr},
-        Error::SentenceNotFinished,
-    };
-    use heapless::String;
-    use nmea0183::{Parser, ParseResult};
+    use gps_miniprojekt::
+        ms5611::{self, Osr}
+    ;
+    use nmea0183::Parser;
     use stm32f4xx_hal::{
         i2c::I2c,
-        pac::{dma1::st::par, I2C1, USART1},
+        pac::{I2C1, USART1},
         serial::Serial,
     };
 
@@ -63,11 +54,8 @@ mod app {
     // Needed even if we don't use it
     #[shared]
     struct Shared {
-        cmd_buffer: heapless::String<82>,
         gnss_data: GnssLocation,
         pressure_data: Ms5611Sample,
-
-
     }
 
     // Holds the local resources (used by a single task)
@@ -75,11 +63,10 @@ mod app {
     #[local]
     struct Local {
         serial: Serial<USART1>,
-        nmea_reciever: NmeaReciever,
-        sender: rtic_sync::channel::Sender<'static, char, BUF_LEN>,
-        receiver: rtic_sync::channel::Receiver<'static, char, BUF_LEN>,
+        ch_sender: rtic_sync::channel::Sender<'static, char, BUF_LEN>,
+        ch_receiver: rtic_sync::channel::Receiver<'static, char, BUF_LEN>,
         ms5611: ms5611::Ms5611<I2c<I2C1>>,
-        parser: Parser,
+        nmea_parser: Parser,
     }
 
     // The init function is called in the beginning of the program
@@ -87,7 +74,7 @@ mod app {
     fn init(ctx: init::Context) -> (Shared, Local) {
         // panic!("test");
         info!("init");
-        defmt::info!("size ={}",size_of::<nmea0183::Parser>());
+        defmt::info!("size ={}", size_of::<nmea0183::Parser>());
 
         // Device specific peripherals
         let mut _device: stm32f4xx_hal::pac::Peripherals = ctx.device;
@@ -102,7 +89,7 @@ mod app {
         let tx = gpioa.pa9.into_alternate::<7>();
         let rx = gpioa.pa10.into_alternate::<7>();
 
-        let (sender, receiver) = rtic_sync::make_channel!(char, BUF_LEN);
+        let (ch_sender, ch_receiver) = rtic_sync::make_channel!(char, BUF_LEN);
         let mut serial = usart2
             .serial(
                 (tx, rx),
@@ -119,17 +106,16 @@ mod app {
         let i2c1 = _device.I2C1;
         let i2c = i2c1.i2c((scl, sda), 100.kHz(), &clocks);
         // Create the MS5611 device:
-        let ms5611 = ms5611::Ms5611::new(i2c, 0x77.into(), Osr::Opt1024.into()).unwrap_or_else(|e| {
-            defmt::error!("Error: {:?}", defmt::Debug2Format(&e));
-            panic!();
-        });
+        let ms5611 =
+            ms5611::Ms5611::new(i2c, 0x77.into(), Osr::Opt1024.into()).unwrap_or_else(|e| {
+                defmt::panic!("Error: {:?}", defmt::Debug2Format(&e));
+            });
         // defmt::info!("{}",size_of::<Result<ms5611::Ms5611<I2c<I2C1>>>>());
-        let parser = Parser::new();
-        
+        let nmea_parser = Parser::new();
 
         // usart2.bwrite_all("Hello, world!\n".as_bytes()).unwrap();
         serial.listen(stm32f4xx_hal::serial::Event::RxNotEmpty);
-        
+
         nmea_handler::spawn().unwrap();
         alt_sampler::spawn().unwrap();
         print_data::spawn().unwrap();
@@ -138,18 +124,15 @@ mod app {
         // gga_saver::spawn().ok();
         (
             Shared {
-                cmd_buffer: String::new(),
                 gnss_data: GnssLocation::default(),
                 pressure_data: Ms5611Sample::default(),
-                // nmea_struct: Nmea::default(),
             },
             Local {
                 serial,
-                nmea_reciever: NmeaReciever::new(),
-                sender,
-                receiver,
+                ch_sender,
+                ch_receiver,
                 ms5611,
-                parser,
+                nmea_parser,
             },
         )
     }
@@ -162,68 +145,43 @@ mod app {
         }
     }
 
-    #[task(binds = USART1, priority=10, local = [serial,sender])]
+    #[task(binds = USART1, priority=10, local = [serial,ch_sender])]
     fn usart_input_handler(ctx: usart_input_handler::Context) {
         let serial = ctx.local.serial;
-        let sender = ctx.local.sender;
+        let queue = ctx.local.ch_sender;
         while let Ok(byte) = serial.read() {
-            sender.try_send(byte as char).unwrap();
+            queue.try_send(byte as char).unwrap();
         }
     }
 
-    #[task(priority = 2, local = [nmea_reciever,receiver,parser], shared = [cmd_buffer, gnss_data])]
+    #[task(priority = 2, local = [ch_receiver,nmea_parser], shared = [gnss_data])]
     async fn nmea_handler(ctx: nmea_handler::Context) {
         defmt::debug!("NMEA handler start");
-        let mut cmd_buf = ctx.shared.cmd_buffer;
-        let queue = ctx.local.receiver;
-        let parser = ctx.local.parser;
+        let queue = ctx.local.ch_receiver;
+        let nmea_parser = ctx.local.nmea_parser;
         let mut gnss_data = ctx.shared.gnss_data;
         loop {
             // await here automatically defers the task after receiving a byte
             while let Ok(byte) = queue.recv().await {
-                if let Some(result) = parser.parse_from_byte(byte as u8) {
+                if let Some(result) = nmea_parser.parse_from_byte(byte as u8) {
                     match result {
-                        Ok(sentence) => {
-                            gnss_data.lock(|d| {
-                                if let Ok(loc) = GnssLocation::try_from(sentence) {
-                                    *d = loc;
-                                };
-                            })
-                        }, // Some other sentences..
-                        Err(e) => {defmt::error!("{}",e)} // Got parse error
+                        Ok(result) => {
+                            gnss_data.lock(|d| d.update(result)).unwrap_or_else(|e| {
+                                // Update error is not critical
+                                defmt::trace!("{}", e);
+                            });
+                        }
+                        Err(e) => {
+                            // Parse error should be logged
+                            defmt::warn!("{}", e)
+                        }
                     }
                 }
             }
         }
     }
 
-    // #[task(priority = 3, shared = [ cmd_buffer])]
-    // async fn nmea_decode(ctx: nmea_decode::Context) {
-    //     let mut buf = ctx.shared.cmd_buffer;
-    //     // let mut nmea_struct = ctx.shared.nmea_struct;
-
-    //     defmt::debug!("NMEA decode");
-    //     let local_sentence = buf.lock(|b| {
-    //         let sentence = b.clone();
-    //         b.clear();
-    //         sentence
-    //     });
-    //     defmt::trace!("NMEA: {:?}", local_sentence.as_str());
-
-    //     // nmea_struct.lock(
-    //         // |nmea_struct| match nmea_struct.parse(local_sentence.as_str()) {
-    //         //     Ok(sentence_type) => {
-    //         //         defmt::debug!("NMEA sentence type: {:?}", sentence_type.as_str());
-    //         //     }
-    //         //     Err(e) => {
-    //         //         // This is very expensive, but we're not expecting many errors
-    //         //         defmt::error!("Error: {}", defmt::Display2Format(&e));
-    //         //     }
-    //         // },
-    //     // );
-    // }
-
-    // Task for printing the location and pressure data together: 
+    // Task for printing the location and pressure data together:
     #[task(priority = 3, shared = [gnss_data, pressure_data])]
     async fn print_data(ctx: print_data::Context) {
         let mut gnss_data = ctx.shared.gnss_data;
@@ -234,35 +192,18 @@ mod app {
                 ms5611_data: pressure_data.lock(|d| *d),
                 gnss_location: gnss_data.lock(|d| *d),
             };
-            defmt::info!("{}",data);
-            Systick::delay_until(t + 1.secs()).await;    
+            defmt::info!("{}", data);
+            Systick::delay_until(t + 1.secs()).await;
         }
-
     }
 
-    // The task functions are called by the scheduler
-    // #[task(shared = [nmea_struct], priority = 1)]
-    // async fn gga_saver(ctx: gga_saver::Context) {
-    //     let mut nmea_struct = ctx.shared.nmea_struct;
-
-    //     loop {
-    //         let t = Systick::now();
-    //         defmt::info!(
-    //             "{:?}",
-    //             nmea_struct.lock(|nmea_struct| GnssLocation::from(nmea_struct))
-    //         );
-    //         Systick::delay_until(t + 1.secs()).await;
-    //     }
-    // }
-
- 
     #[task(priority = 2,local = [ms5611], shared = [pressure_data])]
     async fn alt_sampler(ctx: alt_sampler::Context) {
         // panic!("Sampler");
         let ms5611 = ctx.local.ms5611;
         // let mut nmea_struct = ctx.shared.nmea_struct;
         let mut pressure_data = ctx.shared.pressure_data;
-    
+
         loop {
             let sample = ms5611.read_sample().await.unwrap();
             let t = Systick::now();
